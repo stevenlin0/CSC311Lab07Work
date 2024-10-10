8596d00219c3ff9c8f1a9bef0d55e8a5305cea7a
Steven Lin <lins13@farmingdale.edu>
2024-10-09 23:06:18 -0400

    for "Lab 7 work" assignment

---
+++ src/main/java/ArraysAndStreams2.java
@@ -0,0 +1,69 @@
+// Fig. 17.12: ArraysAndStreams2.java
+// Demonstrating lambdas and streams with an array of Strings.
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ArraysAndStreams2 {
+    public static void main(String[] args) {
+        String[] strings =
+                {"Red", "orange", "Yellow", "green", "Blue", "indigo", "Violet"};
+
+        // display original strings
+        System.out.printf("Original strings: %s%n", Arrays.asList(strings));
+
+        // strings in uppercase
+        System.out.printf("strings in uppercase: %s%n",
+                Arrays.stream(strings)
+                        .map(String::toUpperCase)
+                        .collect(Collectors.toList()));
+
+        // strings less than "n" (case insensitive) sorted ascending
+        System.out.printf("strings less than n sorted ascending: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.compareToIgnoreCase("n") < 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER)
+                        .collect(Collectors.toList()));
+
+        // strings less than "n" (case insensitive) sorted descending
+        System.out.printf("strings less than n sorted descending: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.compareToIgnoreCase("n") < 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                        .collect(Collectors.toList()));
+
+        // find and display strings that start with a vowel and ignoring case
+        System.out.printf("strings starting with a vowel: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.matches("(?i)^[aeiou].*"))
+                        .collect(Collectors.toList())
+        );
+
+        // combine all the strings and then display it
+        System.out.printf("concatenated string: %s%n",
+                Arrays.stream(strings)
+                        .collect(Collectors.joining(", "))
+        );
+
+        // count how many strings have more than five characters and display the number
+        long count = Arrays.stream(strings)
+                .filter(s -> s.length() > 5)
+                .count();
+        System.out.printf("number of strings with more than 5 characters: %d%n", count);
+    }
+}
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
